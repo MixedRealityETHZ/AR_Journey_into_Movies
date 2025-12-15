a81845d97b03f4e10228dd1a00c871575357fb3e
ccaius502@gmail.com <ccaius502@gmail.com>
2025-12-15 08:29:18 +0100

    add ip

--- Assets/MobileARTemplateAssets/Scripts/Server/IPPanelController.cs
+++ Assets/MobileARTemplateAssets/Scripts/Server/IPPanelController.cs
@@ -10,6 +10,8 @@ public class IPPanelController : MonoBehaviour
     public TMP_InputField inputFieldIP;
     public Button confirmButton;
     public Button cancelButton;
+    public Button continueButton;
+    public TMP_Text hintText;
     public LocalizationProgressUI localizationProgressUI;
 
     [Header("Targets to Update")]
@@ -19,10 +21,13 @@ public class IPPanelController : MonoBehaviour
 
     private void Start()
     {
-        remembered_ip = "http://";
+        remembered_ip = "http://172.20.10.5:5000";
         IPPanel.SetActive(false);
+        hintText.gameObject.SetActive(false);
+        continueButton.gameObject.SetActive(false);
         confirmButton.onClick.AddListener(OnConfirm);
         cancelButton.onClick.AddListener(OnCancel);
+        continueButton.onClick.AddListener(OnContinue);
 
         Debug.Log("[IPPanel] Auto ip = " + remembered_ip);
     }
@@ -47,6 +52,13 @@ public class IPPanelController : MonoBehaviour
     }
 
     private void OnConfirm()
+    {
+        confirmButton.gameObject.SetActive(false);
+        cancelButton.gameObject.SetActive(false);
+        hintText.gameObject.SetActive(true);
+        continueButton.gameObject.SetActive(true);
+    }
+    private void OnContinue()
     {
         string ip = inputFieldIP.text.Trim();
         remembered_ip = ip;
